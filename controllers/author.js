b0884ef03d09abc33f425855f96df80c511e969b
MarkusMaal2 <markus.maal@voco.ee>
2023-09-14 10:16:24 +0300

    Displayed author article list

--- controllers/author.js
+++ controllers/author.js
@@ -1,20 +1,26 @@
-const con = require('../utils/db.js')
+const Author = require('../models/author.model')
 const getAuthorArticles = (req, res) => {
-    let get_author = `SELECT name FROM author WHERE id = ${req.params.author_id}`
-    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published' FROM article INNER JOIN author ON article.author_id = author.id WHERE author_id = ${req.params.author_id};`
-    let author = ''
-    con.query(get_author, (err, result) => {
-        if (err) throw err
-        if (result.length > 0) {
-            author = result[0].name
+    let author_name = ''
+    Author.getName(req.params.author_id, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Error occurred retrieving author data"
+            })
+        } else {
+            author_name = data.name
         }
     })
-    con.query(query, (err, result) => {
-        if (err) throw err
-        res.render('author', {
-            articles: result,
-            author: author
-        })
+    Author.getArticles(req.params.author_id, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Error occurred retrieving author articles"
+            })
+        } else {
+            res.render('author', {
+                articles: data,
+                author: author_name
+            })
+        }
     })
 }
 
---
+++ models/author.model.js
@@ -0,0 +1,36 @@
+const con = require("../utils/db")
+
+const Author = (author) => {
+    this.name = author.name
+}
+
+
+Author.getName = (id, result) => {
+    let get_author = `SELECT name FROM author WHERE id = ${id}`
+    con.query(get_author, (err, res) => {
+        if (err) {
+            console.log("error: ", err)
+            result (err, null)
+            return
+        }
+        if (res.length > 0) {
+            result(null, res[0])
+        }
+    })
+}
+
+Author.getArticles = (id, result) => {
+    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published' FROM article INNER JOIN author ON article.author_id = author.id WHERE author_id = ${id};`
+    con.query(query, (err, res) => {
+        if (err) {
+            console.log("error: ", err)
+            result (err, null)
+            return
+        }
+        if (res.length > 0) {
+            result(null, res)
+        }
+    })
+}
+
+module.exports = Author

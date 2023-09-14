cf4d3020622409d1a563f8f7962a61a45f8878b5
MarkusMaal2 <markus.maal@voco.ee>
2023-09-14 09:42:04 +0300

    Displayed an article by slug using the article model

--- controllers/article.js
+++ controllers/article.js
@@ -15,12 +15,15 @@ const getAllArticles = (req, res) => {
 
 // show article by a slug
 const getArticleBySlug = (req, res) => {
-    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published', author.name as 'author', author.id as 'author_id' FROM article INNER JOIN author ON article.author_id = author.id WHERE slug = '${req.params.slug}'`
-    con.query(query, (err, result) => {
-        if (err) throw err
-        res.render('article', {
-            article: result
-        })
+    Article.getBySlug(req.params.slug, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message : err.message || "Error occured retrieving article data"
+            })
+        } else {
+            console.log(data)
+            res.render('article', {article: data})
+        }
     })
 }
 
